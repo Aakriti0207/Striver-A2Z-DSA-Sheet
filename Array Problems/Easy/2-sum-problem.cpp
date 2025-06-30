34da33a7b205ed673f2f4c91d4e36b7dca47d296
Aakriti Arya <aakritiarya2005@gmail.com>
2025-06-30 17:29:51 +0530

    2-sum-problem Better Approach HashMaps

--- Array Problems/Easy/2-sum-problem.cpp
+++ Array Problems/Easy/2-sum-problem.cpp
@@ -16,3 +16,22 @@ public:
 };
 
 //Better Approach- HashMaps
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        unordered_map<int, int> mpp;
+
+        for (int i = 0; i < n; i++) {
+            int left_over = target - nums[i];
+
+            if (mpp.find(left_over) != mpp.end()) {
+                return {mpp[left_over], i};
+            }
+
+            mpp[nums[i]] = i;
+        }
+
+        return {-1, -1};
+    }
+};
