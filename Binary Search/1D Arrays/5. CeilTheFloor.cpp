cbfe755f8bd40b1a5c82dbffd850fde5cee32b3b
Aakriti Arya <aakritiarya2005@gmail.com>
2025-07-16 11:28:18 +0530

    GFG

--- Binary Search/1D Arrays/5. CeilTheFloor.cpp
+++ Binary Search/1D Arrays/5. CeilTheFloor.cpp
@@ -7,10 +7,11 @@
 
 //Return an array of integers denoting the [floor, ceil]. Return -1 for floor or ceiling if the floor or ceiling is not present.
 
+
+//Brute Force 
 class Solution {
   public:
     vector<int> getFloorAndCeil(int x, vector<int> &arr) {
-        // code here
         int n=arr.size();
         vector<int>ans;
         int floor=-1, ceil=-1;
@@ -26,4 +27,49 @@ class Solution {
         ans.push_back(ceil);
         return ans;
     }
+}; // TC-> O(n)
+
+//OPTIMAL APPROACH -> Binary Search
+// We cannot write the code of Floor and Ceil together atonce using binary search- If you want to find both in a single code you'll need O(n) TC.
+
+//Floor
+class Solution {
+  public:
+    vector<int> getFloor(int x, vector<int> &arr) {
+        int n=arr.size();
+        int low=0 , high=n-1 , ans=-1;
+        while(low<=high){
+          int mid= low + (high-low)/2;
+          if(arr[mid] <= x){
+            ans=mid;
+            low=mid+1;
+          }else{
+            high=mid-1;
+          }
+        }
+      return ans;
+    }
 };
+
+//Ceil
+class Solution {
+  public:
+    vector<int> getFloorAndCeil(int x, vector<int> &arr) {
+        int n=arr.size();
+        int low=0 , high=n-1 , ans=-1;
+        while(low<=high){
+          int mid = low + (high-low)/2;
+          if(arr[mid] >= x){
+            ans=mid;
+            high=mid-1;
+          }else{
+            low=mid+1;
+          }
+        }
+      return ans;
+    }
+};
+
+
+
+
