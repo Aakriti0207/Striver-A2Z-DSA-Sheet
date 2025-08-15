32003f5d1b6fab8dd55a720202705a95d3bca54b
Aakriti Arya <aakritiarya2005@gmail.com>
2025-08-15 22:58:59 +0530

    LC 206

---
+++ Linked Lists/Medium Problems of LL/5. Reverse LL.cpp
@@ -0,0 +1,43 @@
+//Reverse Linked List
+
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+//BRUTE FORCE -> Using Stack DS
+  //Stack reverse LL due to its LIFO princple
+
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        stack<int> st;
+        while(temp != NULL){
+            st.push(temp -> val);
+            temp = temp -> next;
+        }
+        temp = head;
+        while(temp != NULL){
+            temp -> val = st.top();
+            st.pop();
+            temp = temp -> next;
+        }
+        return head;
+    }
+}; TC -> O(2n) SC -> O(n)
+
+  //OPTIMAL SOLUTION -> Don't use extra Space
+    //Iterative
+class Solution {
+public:
+    ListNode* reverseList(ListNode* head) {
+        ListNode* temp = head;
+        ListNode* prev = NULL;
+        while(temp != NULL){
+            ListNode* front = temp -> next;
+            temp -> next = prev;
+            prev = temp;
+            temp = front;
+        }
+        return prev;
+    }
+}; TC -> O(n)
+
