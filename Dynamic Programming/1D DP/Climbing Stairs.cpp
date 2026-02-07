dd1883b8dc74dd484674553978eaab54e43a40a2
Aakriti Arya <aakritiarya2005@gmail.com>
2026-02-07 23:12:00 +0530

    lc 70

--- Dynamic Programming/1D DP/Climbing Stairs.cpp
+++ Dynamic Programming/1D DP/Climbing Stairs.cpp
@@ -1,5 +1,18 @@
 /*
 Here, we are standing at the bottom of the stairs and we can climb 1 or 2 stairs at a time
 Return : The number of distinct ways we can reach the stairs
-
 */
+
+Thinking first-
+
+  at step 0 - 0
+  at step 1 - 1 way
+  step 2 - 2 ways, 1 direct climb & 2 climbing step 1 first then step 2
+  step 3 - 3 ways
+  step 4- 5 ways
+
+  OBSERVATION- 
+  ways(3) = ways(2) + ways(1)
+  ways(4) = ways(3) + ways(2)
+
+  Thus, this observation makes this question ENTIRELY similar to fibonacci (Solved in intro)
